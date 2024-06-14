eeac196ae8ab43f7f71d6a3ae70b130620bd866d
Adewole Adewoye <adewoyeadedayo10@gmail.com>
2024-06-14 07:36:50 +0100

    deployment

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:8000", {
+			const socket = io("https://isolated-becka-cliqpod-149c9735.koyeb.app", {
 				query: {
 					userId: authUser._id,
 				},
